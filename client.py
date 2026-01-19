f6adc240d622fffac897da94deaf6b9e49274bf7
Thien1201 <truongthanhthien120105@gmail.com>
2026-01-19 23:59:34 +0700

    Implement TicTacToeClient class
    Add TicTacToeClient class for game functionality

--- client.py
+++ client.py
@@ -5,3 +5,17 @@ from tkinter import messagebox, simpledialog
 
 HOST = '127.0.0.1'
 PORT = 55555
+
+class TicTacToeClient:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Game Tic-Tac-Toe")
+        
+        self.client = None
+        self.my_name = ""
+        self.my_symbol = ""
+        self.opponent_name = ""
+        self.is_my_turn = False
+        
+        self.build_login_ui()
+        self.window.mainloop()
