39138d8279ce8f065325b29bb359e45f7821ffb3
GiaMinhh39 <minhng2978@ut.edu.vn>
2026-01-20 21:52:15 +0700

    add update init

--- server.py
+++ server.py
@@ -9,3 +9,14 @@ server.bind((HOST, PORT))
 server.listen()
 
 print(f"Server đang chạy tại {HOST}:{PORT}...")
+
+class GameSession:
+    def __init__(self, p1_socket, p2_socket, p1_name, p2_name):
+        self.p1 = p1_socket
+        self.p2 = p2_socket
+        self.p1_name = p1_name
+        self.p2_name = p2_name
+        self.board = [""] * 9
+        self.turn = "X"
+        self.rematch_state = {p1_socket: False, p2_socket: False}
+        self.lock = threading.Lock()
