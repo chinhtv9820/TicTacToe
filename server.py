d8316d9590724184b2241b174f45f28f5c63cbe4
GiaMinhh39 <minhng2978@ut.edu.vn>
2026-01-20 21:51:26 +0700

    Port , host

--- server.py
+++ server.py
@@ -1,2 +1,11 @@
 import socket
 import threading
+
+HOST = '127.0.0.1'
+PORT = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+print(f"Server đang chạy tại {HOST}:{PORT}...")
